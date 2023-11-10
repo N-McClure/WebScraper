5dbb7748ab51853b0d614b2a986c2e562c5eeb0d
Nick McClure <64433966+N-McClure@users.noreply.github.com>
2023-11-10 14:30:26 -0500

    edited the main script and functions better than IF statements...Loads better.

--- WebScraper.py
+++ WebScraper.py
@@ -10,15 +10,4 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 SearchedTag = input("Enter the tag of the Site's HTML that you want to Scrape and See: ")
 
-if SearchedTag == "title":
-    print(soup.title)
-elif SearchedTag == "head":
-    print(soup.head)
-elif SearchedTag == "body":
-    print(soup.body)
-elif SearchedTag == "style":
-    print(soup.style)
-elif SearchedTag == "script":
-    print(soup.script)
-elif SearchedTag == "pyscript":
-    print(soup.pyscript)
+print(soup.find_all(SearchedTag))
